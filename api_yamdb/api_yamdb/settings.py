5e0acdc434abace84f177e43c8a253597ecbd70e
Alexander Kolesov <4kolesov@gmail.com>
2022-11-07 14:42:20 +0300

    gggg

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'dev': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
 }
 
 DATABASES['default'] = DATABASES['dev' if DEBUG else 'production']
